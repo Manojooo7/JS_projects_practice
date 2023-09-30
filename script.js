dfeb63e22b7cd2087273ee02a9867bf79a670cf5
Manojooo7 <manojmondal9834@gmail.com>
2023-09-30 23:59:55 +0530

    v2

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterButton = document.getElementById('twitter')
 const newQuoteButton = document.getElementById('new-quote')
+const loader = document.getElementById('loader');
+
 
 // Get quotes from API
 let apiQuotes = [];
 
+// loading Spinner
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+function removeLoadingSpinner() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 //show new quote
 
-function newQuote(){
+function newQuote() {
+    showLoadingSpinner()
     //pick a random quote
 
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
@@ -18,33 +31,37 @@ function newQuote(){
     authorText.textContent = !quote.author ? 'unknown' : quote.author;
 
     // reducing the font size if the quote text content is greater the 120 
-    if (quote.text.length > 120){
+    if (quote.text.length > 120) {
         quoteText.classList.add('long-quote');
-    }else{
+    } else {
         quoteText.classList.remove('long-quote');
     }
+
+    // set quote , hide loader
     quoteText.textContent = quote.text
+    removeLoadingSpinner()
 }
 
-async function getQuotes(){
+async function getQuotes() {
+    showLoadingSpinner()
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
-    try{
-        const respons = await fetch(apiUrl)
-        apiQuotes = await respons.json();
+    try {
+        const response = await fetch(apiUrl)
+        apiQuotes = await response.json();
         newQuote()
-    }catch (error){
-        // cathc error Here
+    } catch (error) {
+        // catch error Here
     }
 }
 
 // tweet quote
 
-function tweetQuote(){
+function tweetQuote() {
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
     window.open(twitterUrl, '_blank')
 }
 
-// Event Listner
+// Event Listener
 
 newQuoteButton.addEventListener('click', newQuote);
 twitterButton.addEventListener('click', tweetQuote)
