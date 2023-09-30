aebe64b781ba7c9744b1d1ebfd798db2bf380bcf
Manojoo7 <manojmondl9834@gmail.com>
2023-09-30 12:26:17 +0530

    base-featured-complete

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// dom selection
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterButton = document.getElementById('twitter')
+const newQuoteButton = document.getElementById('new-quote')
+
 // Get quotes from API
 let apiQuotes = [];
 
@@ -7,7 +14,16 @@ function newQuote(){
     //pick a random quote
 
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-    console.log(quote);
+
+    authorText.textContent = !quote.author ? 'unknown' : quote.author;
+
+    // reducing the font size if the quote text content is greater the 120 
+    if (quote.text.length > 120){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text
 }
 
 async function getQuotes(){
@@ -21,6 +37,17 @@ async function getQuotes(){
     }
 }
 
+// tweet quote
+
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank')
+}
+
+// Event Listner
+
+newQuoteButton.addEventListener('click', newQuote);
+twitterButton.addEventListener('click', tweetQuote)
 
 
 // On load
